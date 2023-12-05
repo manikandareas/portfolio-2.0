85f349cc80b8a3c370e4d93088415001a21f574a
manikandareas <vitoandareas15@gmail.com>
2023-12-05 22:21:26 +0800

    first commit

---
+++ app/common/components/headers/index.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+
+export default function Headers({}: Props) {
+  return <header>Headers</header>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import { cn } from './common/libs/utils'
+import { inter } from './common/libs/fonts'
+import { ThemeProvider } from './common/components/ThemeProvider'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +16,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn("min-h-screen bg-background font-sans antialiased",inter.className)}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        {children}
+        </ThemeProvider>
+        </body>
     </html>
   )
 }
