1c750ce2392c71305e7178ab6f943e5a4e973d04
manikandareas <vitoandareas15@gmail.com>
2023-12-11 01:48:07 +0800

    refactor(opener): refactor opener using typeit

--- app/common/components/Opener.tsx
+++ app/common/components/Opener.tsx
@@ -1,19 +1,15 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import AnimatedTitle from "./AnimatedTitle";
-import { AnimatePresence, motion } from "framer-motion";
-import { cn } from "../libs/utils";
-import { firaCode } from "../libs/fonts";
+import { AnimatePresence, delay, motion } from "framer-motion";
+
+import TypeIt from "typeit-react";
 
 export default function Opener() {
   const [openerIsOpen, setOpenerIsOpen] = useState<boolean>(false);
+
   useEffect(() => {
     setOpenerIsOpen(true);
-
-    setTimeout(() => {
-      setOpenerIsOpen(false);
-    }, 3000);
   }, []);
   return (
     <AnimatePresence>
@@ -22,21 +18,32 @@ export default function Opener() {
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: -100 }}
-          className="fixed w-screen h-screen flex items-center justify-center bg-zinc-950 space-y-2 z-[9999] flex-col px-4 text-center"
+          className="fixed w-screen h-screen flex items-center justify-center bg-zinc-950 space-y-2 z-[9999] flex-col px-4 text-center text-6xl font-semibold"
         >
-          <AnimatedTitle text="Vito&nbsp;Andareas&nbsp;Manik" />
-          <motion.div
-            initial={{ opacity: 0, y: -500 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 500 }}
-            transition={{ delay: 1, type: "spring", bounce: 0.5 }}
-            className={cn(
-              firaCode.className,
-              "font-serif text-7xl text-center font-bold text-neutral-50"
-            )}
-          >
-            Welcome!
-          </motion.div>
+          <TypeIt
+            options={{
+              afterComplete: () => delay(() => setOpenerIsOpen(false), 1000),
+            }}
+            getBeforeInit={(instance) => {
+              instance
+                .type("Viti Andares", { delay: 100 })
+                .move(-1, { delay: 100 })
+                .type("a", { delay: 400 })
+                .move(-8, { delay: 100 })
+                .delete(1)
+                .type("o", { delay: 100 })
+                .move(null, { to: "END" })
+                .type('<em><strong class="manik"> Malik.</strong></em>', {
+                  speed: 100,
+                })
+                .pause(500)
+                .delete(".manik", { delay: 800, instant: true })
+                .type('<em><strong class="manik"> Manik.</strong></em>', {
+                  speed: 100,
+                });
+              return instance;
+            }}
+          />
         </motion.div>
       ) : null}
     </AnimatePresence>
