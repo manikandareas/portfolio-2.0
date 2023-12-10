fbd7baae5f93fa5c8bacc364e3c4473ee259d279
manikandareas <vitoandareas15@gmail.com>
2023-12-11 02:12:37 +0800

    refactor(opener): move to separate component

--- app/common/components/AnimatedTitle.tsx
+++ app/common/components/AnimatedTitle.tsx
@@ -1,83 +1,35 @@
-import { useEffect } from "react";
+import { delay } from "framer-motion";
+import TypeIt from "typeit-react";
 
-import styled from "@emotion/styled";
-
-import { useAnimation, motion } from "framer-motion";
-
-import { useInView } from "react-intersection-observer";
-import { firaCode } from "../libs/fonts";
-
-const Title = styled.h2`
-  font-size: 3rem;
-
-  font-weight: 600;
-`;
-
-const Character = styled(motion.span)`
-  display: inline-block;
-
-  font-family: ${firaCode.style.fontFamily};
-  margin-right: -0.05em;
-`;
-
-export default function AnimatedTitle({ text }: { text: string }) {
-  // This would normally be passed into this component as a prop!
-
-  const ctrls = useAnimation();
-
-  const { ref, inView } = useInView({
-    threshold: 0.5,
-
-    triggerOnce: true,
-  });
-
-  useEffect(() => {
-    if (inView) {
-      setTimeout(() => {
-        ctrls.start("visible");
-      }, 500);
-    }
-
-    if (!inView) {
-      ctrls.start("hidden");
-    }
-  }, [ctrls, inView]);
-
-  const characterAnimation = {
-    hidden: {
-      opacity: 0,
-
-      y: `0.25em`,
-    },
-
-    visible: {
-      opacity: 1,
-
-      y: `0em`,
-
-      transition: {
-        duration: 1,
-
-        ease: [0.2, 0.65, 0.3, 0.9],
-      },
-    },
-  };
-
+export default function AnimatedTitle({
+  action,
+}: {
+  action: (value: React.SetStateAction<boolean>) => void;
+}) {
   return (
-    <Title aria-label={text} role="heading">
-      {text.split("").map((character, index) => (
-        <Character
-          className="font-serif text-neutral-50 text-center"
-          ref={ref}
-          aria-hidden="true"
-          key={index}
-          initial="hidden"
-          animate={ctrls}
-          variants={characterAnimation}
-        >
-          {character}
-        </Character>
-      ))}
-    </Title>
+    <TypeIt
+      options={{
+        afterComplete: () => delay(() => action(false), 1000),
+      }}
+      getBeforeInit={(instance) => {
+        instance
+          .type("Viti Andares", { delay: 100 })
+          .move(-1, { delay: 100 })
+          .type("a", { delay: 400 })
+          .move(-8, { delay: 100 })
+          .delete(1)
+          .type("o", { delay: 100 })
+          .move(null, { to: "END" })
+          .type('<em><strong class="manik"> Malik.</strong></em>', {
+            speed: 100,
+          })
+          .pause(500)
+          .delete(".manik", { delay: 800, instant: true })
+          .type('<em><strong class="manik"> Manik.</strong></em>', {
+            speed: 100,
+          });
+        return instance;
+      }}
+    />
   );
 }
--- app/common/components/Opener.tsx
+++ app/common/components/Opener.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { AnimatePresence, delay, motion } from "framer-motion";
 
 import TypeIt from "typeit-react";
+import AnimatedTitle from "./AnimatedTitle";
 
 export default function Opener() {
   const [openerIsOpen, setOpenerIsOpen] = useState<boolean>(false);
@@ -18,32 +19,9 @@ export default function Opener() {
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: -100 }}
-          className="fixed w-screen h-screen flex items-center justify-center bg-zinc-950 space-y-2 z-[9999] flex-col px-4 text-center text-6xl font-semibold"
+          className="fixed w-screen h-screen flex items-center justify-center bg-zinc-950/80 backdrop-blur space-y-2 z-[9999] flex-col px-4 text-center lg:text-6xl text-4xl font-semibold font-mono"
         >
-          <TypeIt
-            options={{
-              afterComplete: () => delay(() => setOpenerIsOpen(false), 1000),
-            }}
-            getBeforeInit={(instance) => {
-              instance
-                .type("Viti Andares", { delay: 100 })
-                .move(-1, { delay: 100 })
-                .type("a", { delay: 400 })
-                .move(-8, { delay: 100 })
-                .delete(1)
-                .type("o", { delay: 100 })
-                .move(null, { to: "END" })
-                .type('<em><strong class="manik"> Malik.</strong></em>', {
-                  speed: 100,
-                })
-                .pause(500)
-                .delete(".manik", { delay: 800, instant: true })
-                .type('<em><strong class="manik"> Manik.</strong></em>', {
-                  speed: 100,
-                });
-              return instance;
-            }}
-          />
+          <AnimatedTitle action={setOpenerIsOpen} />
         </motion.div>
       ) : null}
     </AnimatePresence>
